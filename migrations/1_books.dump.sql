81c46bc8c71f47b828aaf32c090589c11d4bcfb4
iman tung <iman.tung@gmail.com>
2021-11-06 11:04:49 +0700

    add postgres version

--- migrations/1_books.dump.sql
+++ migrations/1_books.dump.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.3 (Debian 13.3-1.pgdg100+1)
+-- Dumped from database version 13.4 (Debian 13.4-4.pgdg110+1)
 -- Dumped by pg_dump version 13.3
 
 SET statement_timeout = 0;
